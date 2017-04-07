8a59d7207468840e5f4723d536a8e938e10ff540
Alessandro Di Maria <adm@m42.ch>
2017-04-07 17:05:56 +0200

    queue as gen server

--- lib/event_cast/queue.ex
+++ lib/event_cast/queue.ex
@@ -1,12 +1,27 @@
 defmodule EventCast.Queue do
+  use GenServer
 
-  # def start_link do
-  #   #EventCast.Queue.enqueue
-  # end
+  def start_link do
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
 
-  def enqueue do
-    event = %Event{payload: "Hello World"}
-    EventCast.Worker.fire(event, &(String.reverse(&1)))
+  def handle_cast({:enqueue, event} , events) do
+    { :noreply, [event|events] }
+  end
+
+  def handle_call(:next, _client, [event|tail]) do
+    future = Task.async(EventCast.Worker, :fire, [event, &(String.reverse(&1))])
+    result = Task.await(future)
+    IO.puts("result: #{result}")
+    { :reply, result, [tail] }
+  end
+
+  def enqueue(event) do
+    GenServer.cast(__MODULE__, {:enqueue, event})
+  end
+
+  def next do
+    GenServer.call(__MODULE__, :next)
   end
 
 end
